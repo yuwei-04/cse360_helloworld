53eca15df771647c3d45e04b72ac1bf627765171
yuwei <yuweiwu1@asu.edu>
2025-10-17 23:05:07 -0700

    Refactor arithmetic functions and add square function
    Refactor arithmetic functions for consistency and add square function.

--- calc.py
+++ calc.py
@@ -1,8 +1,13 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
-def subtract(a,b):
-    return a-b
+def subtract(a, b):
+    return a - b
 
-def mul(a,b):
-    return a*b
+def mul(a, b):
+    return a * b
+
+def square(x):
+    """Return x*x."""
+    return x * x
+
