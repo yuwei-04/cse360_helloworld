6a1f4124096fa1d182a88e85fdab4d72c9e48f16
yuwei <yuweiwu1@asu.edu>
2025-10-17 23:05:21 -0700

    Add square test and format existing tests
    Added test for the square function and improved formatting.

--- test_calc.py
+++ test_calc.py
@@ -1,10 +1,13 @@
-from src.calc import add, subtract, mul
+from src.calc import add, subtract, mul, square
 
 def test_add():
-    assert add(2,3)==5
+    assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(5,3)==2
+    assert subtract(5, 3) == 2
 
 def test_mul():
-    assert mul(4,3)==12
+    assert mul(4, 3) == 12
+
+def test_square():
+    assert square(5) == 25
